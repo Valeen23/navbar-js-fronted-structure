2235b5f3ce939865fc680d4780eea231e06e3baf
Valentina Toro <valen231120@gmail.com>
2025-07-07 19:04:26 -0500

    feat: Se agregó el hero, se mejoraron los estilos del navbar

--- frontend/public/js/components/navbar.js
+++ frontend/public/js/components/navbar.js
@@ -5,9 +5,29 @@ document.addEventListener("DOMContentLoaded", function(){
         fetch("/frontend/public/views/components/navbar.html")
         .then(response => response.text())
         .then(data => {navbarElement.innerHTML = data;
-    })
 
+        //===logica para reslatar el enlace que está activo en el navbar
+
+        //Obtener la ruta actual
+
+        //Si no hay un archivo específico se asume que es el index
+        const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+        //Seleciona todos los enlaces del navbar que usarán la clase personalizada
+        const navbarLinks = navbarElement.querySelectorAll(".navbar__link");
+
+        //Recorre cada enlace del navbar
+        navbarLinks.forEach(link => {
+
+            //Verifica si el href del enlace incluye el nombre de la página actual
+            if(link.getAttribute("href").includes(currentPage)){
+
+                //Si es la página actual se le asigna la clase 'activa' para destacarla visulamente 
+                link.classList.add("active");
+            }
+        });
+
+    })
     .catch(error => console.error("Error cargando el navbar", error));
-    } 
-    
+    }
 });
